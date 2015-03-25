ed806c442395e88edfbbf0393391f3c52c4ea977
MudarGit <kneehighmud@gmail.com>
2015-03-25 11:34:48 -0400

    Income Tax Calculator

--- Lab32515/Program.cs
+++ Lab32515/Program.cs
@@ -10,6 +10,32 @@ namespace Lab32515
     {
         static void Main(string[] args)
         {
+            int income;
+            string value = args[0];
+            income = Convert.ToInt32(value);
+            int taxes;
+
+            if(income <=20000)
+            {
+                taxes = income * 1 / 20;
+                Console.WriteLine(taxes);
+            }
+            if(income >20000 && income <= 50000)
+            {
+                taxes = income * 1 / 10;
+                Console.WriteLine(taxes);
+            }
+            if(income >50000 && income <= 75000)
+            {
+                taxes = income * 1 / 5;
+                Console.WriteLine(taxes);
+            }
+            if (income > 75000)
+            {
+                taxes = income * 35 / 100;
+                Console.WriteLine(taxes);
+            }
+
         }
     }
 }
